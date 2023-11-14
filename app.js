7d43c079a887052f62116fdcd5f2a14d60821fa3
vildan <vcetin1356@gmail.com>
2023-11-14 15:21:44 +0300

    add js part finished

--- app.js
+++ app.js
@@ -1,70 +1,121 @@
 // ? ADD DATE-AMOUNT-AREA PART TO TABLE
-const infos = [];
-const revenueArr = [];
 
-const walletForm = document
-  .querySelector(".walletForm")
-  .addEventListener("submit", (event) => {
+let infos = [];
+let revenues=0;
+
+// ? SELECTORS
+
+// * Revenue form
+
+const revenue = document.querySelector("#revenue");
+const revenueForm = document.querySelector(".revenueForm")
+const checkBtn= document.querySelector(".check-btn")
+
+
+// * Spend form
+
+const walletForm = document.querySelector(".walletForm")
+const date = document.querySelector("#date");
+const amount = document.querySelector("#amount")
+const area = document.querySelector("#area");
+
+
+// *Result table
+
+let expense = document.getElementById("expense");
+let rest = document.getElementById("rest");
+let revenueT = document.querySelector("#total");
+
+// * Spend table
+
+const spendBody=document.querySelector("#spend-body")
+const clearBtn=document.getElementById("clear-btn")
+
+// ? CREATE OBJECT
+// ? create object with entered values and save to localstorage
+walletForm.addEventListener("submit", (event) => {
     event.preventDefault();
     console.log(event.target);
 
-    const date = document.querySelector("#date").value;
-    const amount = Number(document.querySelector("#amount").value);
-    const area = document.querySelector("#area").value;
-
-    document.querySelector(".walletForm").reset();
-    addElement(date, amount, area);
+    let info = { 
+      id:new Date().getTime(),
+      date: date.value, 
+      amountMoney: Number(amount.value), 
+      spendingArea: area.value };
+      walletForm.reset();
+    infos.push(info);
+    localStorage.setItem("infos", JSON.stringify(infos));
+    writeToDOM(info)
+    countAndUpdate()
+    
   });
 
 // ? REVENUE PART
-const revenueForm = document
-  .querySelector(".revenueForm")
-  .addEventListener("submit", (event) => {
+// ? save entered revenues to localstorage 
+revenueForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    const revenue = Number(document.querySelector("#revenue").value);
-
-    document.querySelector(".revenueForm").reset();
-    addResult(revenue);
+    revenues = revenues + Number(revenue.value)
+    localStorage.setItem("revenue",revenues);    
+    revenueT.textContent = formattedCurrency(revenues);
+    revenueForm.reset();
+    countAndUpdate()
+    
   });
 
+  // ? PAGE LOAD
+  // ? when page load get an array from localstorage and write to DOM  
 window.addEventListener("load", () => {
-  let info = JSON.parse(localStorage.getItem("info"));
-
-  if (info) {
-    displayInfo(info);
-  }
+  infos = JSON.parse(localStorage.getItem("infos")) || [];
+  infos.forEach((info)=>{writeToDOM(info)})
+  date.valueAsDate=new Date()
+  countAndUpdate()
+  
 });
+
 // ? ADD NEW ELEMENT TO TABLE
-function addElement(date, amount, area) {
+// ? this function take parameter as an object and create table elments and writes to DOM
+function writeToDOM({id,date,amountMoney,spendingArea}) {
   let row = document.createElement("tr");
+  console.log(amountMoney)
+  const appendTd=(content)=>{
+    const td=document.createElement("td")
+    td.textContent=content
+    return td
+  }
+  const formatted=(amount)=>{
+    const td=document.createElement("td")
+    td.textContent=formattedCurrency(Number(amount))
+    return td
+  }
+  //  ? create delete item
+  const lastTd=()=>{
+    const td = document.createElement("td")
+    const iElement=document.createElement("i")
+    iElement.id=id
+    iElement.className="fa-solid fa-trash-can text-danger"
+    iElement.type="button"
+    td.appendChild(iElement)
+    return td
+  }
 
-  let dateT = document.createElement("td");
-  let amountT = document.createElement("td");
-  let areaT = document.createElement("td");
-  let process = document.createElement("td");
+  row.append(
+    appendTd(date),
+    appendTd(spendingArea),
+    formatted(amountMoney),
+    lastTd()
+  )
+  // ? add to table body
+  spendBody.prepend(row);
 
-  dateT.textContent = date;
-  amountT.textContent = formattedCurrency(amount);
-  areaT.textContent = area;
-  process.textContent = "@";
-
-  row.append(dateT, amountT, areaT, process);
-  document.querySelector("#spend-body").appendChild(row);
-
-  let info = { date: date, amountMoney: amount, spendingArea: area };
-  infos.push(info);
-  localStorage.setItem("info", JSON.stringify(infos));
+ 
 }
 
-function addResult(revenue) {
-  let revenueT = document.querySelector("#total");
-  revenueT.textContent = formattedCurrency(revenue);
-
-  revenueArr.push(revenue);
-  localStorage.setItem("revenue", JSON.stringify(revenueArr));
-
-  let expense = document.getElementById("expense");
-  let totalExpense = JSON.parse(localStorage.getItem("info")).reduce(
+// ? COUNT AND UPDATE
+// ? calculate all expense and show the rest money 
+function countAndUpdate() {
+  revenues=Number(localStorage.getItem("revenue")) || 0
+  revenueT.textContent=formattedCurrency(revenues)
+  let totalExpense = JSON.parse(localStorage.getItem("infos")).reduce(
     (acc, val) => {
       return acc + val.amountMoney;
     },
@@ -73,11 +124,10 @@ function addResult(revenue) {
   expense.textContent = formattedCurrency(totalExpense);
   console.log(totalExpense);
 
-  let rest = document.getElementById("rest");
-  rest.textContent = formattedCurrency(revenue - totalExpense);
+  rest.textContent = formattedCurrency(revenues - totalExpense);
 }
 
-// Formatting as currency
+// ?  Formatting as currency
 function formattedCurrency(number) {
   let formattedNumber = number.toLocaleString("en-US", {
     style: "currency",
@@ -86,26 +136,35 @@ function formattedCurrency(number) {
   return formattedNumber;
 }
 
-function displayInfo(info) {
+// ? REMOVE ELEMENT
+// ? when click to delete item it remove the table row and update localstorage and expenses
+spendBody.addEventListener("click",(event)=>{
+  console.log(event.target)
+  if(event.target.classList.contains("fa-trash-can")){
+    // console.log(event.target.closest("tr"))
+    event.target.parentElement.parentElement.remove()
+    event.target.closest("tr").remove()
+  }
 
-  info.forEach((element) => {
 
-  let row = document.createElement("tr");
-    let dateT = document.createElement("td");
-    let amountT = document.createElement("td");
-    let areaT = document.createElement("td");
-    let process = document.createElement("td");
+  const id=event.target.id
 
-    console.log(element);
-    row.append(dateT, amountT, areaT, process);
-    dateT.textContent = element.date;
-    amountT.textContent = formattedCurrency(element.amountMoney);
-    areaT.textContent = element.spendingArea;
-    process.textContent = "@";
+  infos=infos.filter((i)=>i.id!=id)
 
-     document.querySelector("#spend-body").appendChild(row);
-    
-  });
-  
- 
-}
+  localStorage.setItem("infos",JSON.stringify(infos))
+
+  countAndUpdate()
+})
+
+// ? DELETE ALL INFORMATION
+// ? delete all info 
+clearBtn.addEventListener("click",()=>{
+  if(confirm("Are you sure to delete?")){
+    infos=[]
+    revenues=0
+    localStorage.removeItem("infos")
+    localStorage.removeItem("revenue")
+    spendBody.innerHTML=""
+    countAndUpdate()
+  }
+})
